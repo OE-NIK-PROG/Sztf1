afeb224aada9ddf2c2497d3d8db0a1f46f47a0ef
vekonymartin <vekony.martin15@gmail.com>
2023-09-25 22:56:32 +0200

    Updated

---
+++ 3. Labor/C#/Labor3_a.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace HarmadikLabor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // -------------------------------------------------------------------------------------------------------------
+            //      ALAP DOLGOK
+            // -------------------------------------------------------------------------------------------------------------
+            
+
+            // deklarálás és létrehozás
+
+
+            // értékadás
+
+            // -----> evszamok, nevek --> vegyük észre, hogy van egy teljesen jól használható tároló eszközöm
+
+            // ugyan úgy mint sima változó esetében
+            // felül tudjuk írni a tartalmát
+
+
+            // érték "lekérdezése"
+
+
+            // matematikai művelet elvégzése is lehetséges közvetlenül
+
+            
+            
+            // -------------------------------------------------------------------------------------------------------------
+            //      HIBÁK
+            // -------------------------------------------------------------------------------------------------------------
+
+            
+
+            // -------------------------------------------------------------------------------------------------------------
+            //      ÉRDEKESSÉGEK
+            // -------------------------------------------------------------------------------------------------------------
+            
+
+            // 1. Hozzunk létre tömböt, aminek egy változó mondja meg, hogy mekkora legyen a mérete.
+
+            // 2. Hozzunk létre tömböt, tetszőleges darabszámmal, amit a felhasználó adhat meg.
+
+
+            // 3. Írjuk ki a tömb tartalmát a 2. laboron tanult ciklussal.
+
+        }
+    }
+}
---
+++ 3. Labor/C#/Labor3_b.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Random szám létrehozása
+            
+            // 1.) Generáljunk x db random számot for ciklussal. 
+
+            
+            // 2.) Töltsünk fel random számokkal egy tömböt, for ciklust használva. 
+            // tömb hosszának lekérzése: tomb.Length
+
+            
+            // 3.) Készítsünk egy tömböt, 100 elemmel. 
+            // Minden második helyre 7777-et írjunk be, majd külön ciklussal írassuk is ki. 
+
+            // kiíratás         
+            
+            // 4.) Tömböt töltsük fel véletlen számokkal. 
+            
+
+            // 5.) Határozzuk meg az elemek összegét.
+
+            
+            // 6.) Határozzuk meg az elemek átlagát. 
+
+            
+            // 7.) Határozzuk meg a páratlan elemek átlagát. 
+
+            
+            // 8.) Szerepel-e 5-tel osztható szám a tömbben? Ha igen, melyik helyen? 
+            //  (!!!) Hivatalosan ezt az Eldöntés programozási tétellel kellene/illene megvalósítani, amely
+            // WHILE ciklust követelne meg, azonban most a gyakorlás kedvéért FOR ciklussal csináljuk.
+
+            
+            // 9.) Hány darab 3-mal osztható szám van a tömbben?
+
+
+            // 10. Gyűjtsük ki az összes páros számot. (új tömb használat) 
+            
+        }
+    }
+}
