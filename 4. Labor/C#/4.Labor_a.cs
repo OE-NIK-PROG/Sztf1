d85eecf20bbe4a51cd72346e93f6ee0f382b6071
vekonymartin <vekony.martin15@gmail.com>
2023-10-02 21:04:45 +0200

    Updated

---
+++ 4. Labor/C#/4.Labor_a.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Internal;
+
+namespace Pokemon_csata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // tömb létrehozása
+           
+
+
+            // For ciklussal hogyan is néz ki a kiíratás?
+            
+
+            
+            // Foreach ciklussal hogyan nézne ki ugyan ez?
+            
+
+            // FELADATOK MEGVALÓSÍTÁSA FOREACH CIKLUSSAL
+            /*
+             * Az itt következő feladatok ugyan azok, mint amiket a 3. laboron vettünk for ciklussal.
+             * Viszont gyakorlásképpen csináljuk meg, hogy lássuk mi a különbség (pozitívum és 		     * negatívum).
+             * 
+             */
+
+
+
+            // 1.) Generáljunk x db random számot foreach ciklussal.
+
+            // Mivel a foreach csak valamilyen adatstruktúrán -jelenleg tömbön- tud végigmenni,
+            // ezért ha ilyen nem áll rendelkezésre, nem tudunk kivitelezni vele
+            // "számláló" ciklust...
+
+
+
+            // 2.) Töltsünk fel random számokkal egy tömböt, foreach ciklust használva.
+
+            // látható, hogy csak adat kiolvasásra használható jól igazán
+            // módosításra, majd visszaírásra nem
+           
+
+            // 5.) Határozzuk meg az elemek összegét.
+           
+
+            // 6.) Határozzuk meg az elemek átlagát.
+            
+
+            // vagy közvetlen elosztjuk a kiíratáskor: 
+
+
+            // 7.) Határozzuk meg a páratlan elemek átlagát.
+          
+
+
+            // 8.) Szerepel-e 5-tel osztható szám a tömbben? Ha igen, melyik helyen?
+
+            // itt már sokkal sokkal célszerűbb lenne for ciklus használata
+            // lévén, hogy index-et szeretnénk megkapni...
+
+            //  (!!!) Hivatalosan ezt az Eldöntés programozási tétellel kellene/illene megvalósítani, amely
+            // WHILE ciklust követelne meg, azonban most a gyakorlás kedvéért FOREACH ciklussal csináljuk.
+
+
+            // 9.) Hány darab 3-mal osztható szám van a tömbben?
+            
+
+
+            // 10. Gyűjtsük ki az összes páros számot. (új tömb használat)
+            
+
+        }
+    }
+}
---
+++ 4. Labor/C#/4.Labor_b.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Matrix
+{
+    class Program
+    {
+
+        // metódusok ugyan úgy működnek 2D tömbök esetén mint sima tömbbel vagy változóval
+
+	// methods, ami visszaad egy mátrixot véletlen számok segítségével
+        
+
+        // visszaadunk egy string típusú mátrixot tele X-el
+        
+
+        // kiírjuk a mátrix tartalmát
+        
+        
+	// kiírjuk a mátrix tartalmát
+        
+
+        static void Main(string[] args)
+        {
+            
+
+            // hivatkozás elemekre
+           
+
+            // értékadás valamelyik elemnek
+            
+
+            // elemek számának lekérdezése
+            
+
+            // feltöltés
+             
+
+            // MÁTRIX BEJÁRÁSA (KÉT FOR CIKLUSSAL) 
+
+        }
+    }
+}
